9f1c42acab6d0a3f7853b399709d75cd4ca10d61
Benny1117Yen <P86071244@gs.ncku.edu.tw>
2019-11-17 05:27:53 +0800

    Quiz1 Fixed.

--- quiz1_fix.php
+++ quiz1_fix.php
@@ -1,9 +1,8 @@
 <?php
-/*
-先解釋補齊一些變數，以及 function 來幫助 try & error;
-過程中的 echo 多半是用來 debug 用。可以注解掉。
-*/
 
+/*
+這裡做 function interface.
+*/
 interface ArticleInterface {
     public function getArticle($blog);
     // public function Exception();
@@ -14,6 +13,9 @@ interface BlogInterface {
     // public function Exception();
 }
 
+/*
+Function implements from interface.
+*/
 class FunctionInterface1 implements ArticleInterface {
     public function getArticle($blog) {
         if ($blog = "PJs") {
@@ -34,6 +36,9 @@ class FunctionInterface2 implements BlogInterface {
     }
 }
 
+/*
+Scope resolution operator 需要用到的 static function.
+*/
 class User {
     static function getUser($user_id) {
         $root = "root";
@@ -42,6 +47,9 @@ class User {
     }
 }
 
+/*
+Exception handling.
+*/
 function checkArticle($status) {
     if ($status) {
         throw new Exception("There is no article here!.".'<br />');
@@ -63,49 +71,42 @@ function checkUser($status) {
     return true;
 }
 
-/* try {
-    check(1);
-}
-
-catch(Exception $e) {
-    echo 'Message: '.$e -> getMessage();
-}
-*/
-
 /*
-這裡我沒有特別寫 getUser funcion，而是直接 call get_current_user()。
+Fixed function.
 */
-// echo 'Current script owner: ' . get_current_user();
 function getUserArticles($user_id, $article_id) { 
     echo $user_id.', '.$article_id.'<br />'; // SYSTEM, 0123
     if ($user_id && $article_id) {
         if ($user = User::getUser($user_id)) { // Class User 內去取用 getUser 這個 function
             $blog = new FunctionInterface2();
-            $blockname = $blog -> sendBlog($user); // PJs
+            $blockname = $blog -> sendBlog($user);
             if ($blockname != 0) {// 這邊改寫一下 $user -> blog，因為還在研究 MVC in Laravel，blog 建立之後會補上。
                 $article = new FunctionInterface1();
-                $articlename = $article -> getArticle($blog); // Quiz1
+                $articlename = $article -> getArticle($blog);
                 if ($articlename != 0) {
                     return $articlename;
                 }
                 else {
                     // echo "There is no article!".'<br />';
-                    checkArticle(1);
+                    checkArticle($articlename);
                 }
             }
             else {
                 // echo "There is no blog!".'<br />';
-                checkBlog(1);
+                checkBlog($blockname);
             }
         }
         else {
             // echo "There is no user!<br />";
-            checkUser(1);
+            checkUser($user);
         }
     }
     return null;
 }
 
+/*
+Argument generator.
+*/
 $user_id = get_current_user();
 $article_id = "0123";
 getUserArticles($user_id, $article_id);
