bbd2b3f0350e45a884c03226ba55f149b3a6a2d3
Benny1117Yen <P86071244@gs.ncku.edu.tw>
2019-11-18 13:43:46 +0800

    Quiz4 TestCase updated.

--- tests/quiz4.php
+++ tests/quiz4.php
@@ -1,15 +1,42 @@
 <?php
 
+require('./src/quiz2_fix_final.php');
 use PHPUnit\Framework\TestCase;
 
-class ClientTest extends TestCase {
-    public function __construct() {
-        $this -> c = new ChinaFreight();
-        $this -> t = new TaiwanFreight();
-        $this -> tAdapter = new TaiwanAdapter($this -> t);
+// ChinaFreight Unittest，可能沒什麼必要，因為測加減乘除，除非硬體問題，不然不會出錯。
+class ChinaFreightTest extends TestCase {
+    /* 來源
+    private $element = 200;
+    private $weight = 60;
+    private $rate = 20;
+    */
 
-        echo "China: ".$this -> c -> request()."<br />";
-        echo "Taiwan: ".$this -> tAdapter -> request();
+    // TestResult: 1200 does not match expected type "object".
+    public function testRequest($element = 200, $weight = 50, $rate = 20) {
+        // return $this -> requestTotal(); 來源
+        $total = new ChinaFreight();
+        $total -> request();
+        // TestCase
+        // $element = 200;
+        // $weight = 50;
+        // $rate = 20;
+        $freight = $element + $weight * $rate;
+        // Assert
+        $this -> assertEquals($total, $freight);
+    }
+
+    // TestResult: 1200 does not match expected type "object".
+    public function testRequestTotal($element = 200, $weight = 50, $rate = 20) {
+        // return $this -> element; 來源
+        $total = new ChinaFreight();
+        $total -> requestTotal();
+        // TestCase
+        // $element = 200;
+        // $weight = 50;
+        // $rate = 20;
+        $freight = $element + $weight * $rate;
+        // Assert
+        $this -> assertEquals($total, $freight);
     }
 }
 ?>
